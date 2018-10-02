51941150d1fa235acb2f91dc2b59223176d823c3
Anton Bukov <k06aaa@gmail.com>
2018-10-03 00:13:37 +0300

    Fix eslint problems

--- software/smartContract/test/PlasmaBlocks.js
+++ software/smartContract/test/PlasmaBlocks.js
@@ -17,7 +17,7 @@ contract('PlasmaBlocks', function ([_, wallet1, wallet2, wallet3, wallet4, walle
         plasma = await PlasmaBlocks.new();
     });
 
-    it('should submit blocks sequentially', async function() {
+    it('should submit blocks sequentially', async function () {
         //
         // [1] [2] [3]
         //             [4]
@@ -25,14 +25,14 @@ contract('PlasmaBlocks', function ([_, wallet1, wallet2, wallet3, wallet4, walle
         //                         [ ]
         //                         [7] [8] [9]
         //
-        await plasma.submitBlocks(0, [1,2,3]);
+        await plasma.submitBlocks(0, [1, 2, 3]);
         await plasma.submitBlocks(3, [4]);
-        await plasma.submitBlocks(4, [5,6]);
+        await plasma.submitBlocks(4, [5, 6]);
         await plasma.submitBlocks(6, []);
-        await plasma.submitBlocks(6, [7,8,9]);
+        await plasma.submitBlocks(6, [7, 8, 9]);
     });
 
-    it('should submit blocks with intersections', async function() {
+    it('should submit blocks with intersections', async function () {
         //
         // [1] [2] [3]
         //         [3] [4]
@@ -41,55 +41,55 @@ contract('PlasmaBlocks', function ([_, wallet1, wallet2, wallet3, wallet4, walle
         //             [4] [5] [6]
         //                 [5] [6] [7] [8] [9]
         //
-        await plasma.submitBlocks(0, [1,2,3]);
-        await plasma.submitBlocks(2, [3,4]);
-        await plasma.submitBlocks(3, [4,5,6]);
+        await plasma.submitBlocks(0, [1, 2, 3]);
+        await plasma.submitBlocks(2, [3, 4]);
+        await plasma.submitBlocks(3, [4, 5, 6]);
         await plasma.submitBlocks(6, []);
-        await plasma.submitBlocks(3, [4,5,6]);
-        await plasma.submitBlocks(5, [5,6,7,8,9]);
+        await plasma.submitBlocks(3, [4, 5, 6]);
+        await plasma.submitBlocks(5, [5, 6, 7, 8, 9]);
     });
 
-    it('should fail to submit old blocks', async function() {
+    it('should fail to submit old blocks', async function () {
         //
         // [1] [2] [3] [4]
         //     [2] [3]
         //
-        await plasma.submitBlocks(0, [1,2,3,4]);
-        await plasma.submitBlocks(1, [2,3]).should.be.rejectedWith(EVMThrow);
+        await plasma.submitBlocks(0, [1, 2, 3, 4]);
+        await plasma.submitBlocks(1, [2, 3]).should.be.rejectedWith(EVMThrow);
     });
 
-    it('should ignore blocks after gap', async function() {
+    it('should ignore blocks after gap', async function () {
         //
         // [1] [2] [3]
         //             [ ]
         //                 [5] [6]
         //
-        await plasma.submitBlocks(0, [1,2,3]);
-        await plasma.submitBlocks(4, [5,6]).should.be.rejectedWith(EVMThrow);
+        await plasma.submitBlocks(0, [1, 2, 3]);
+        await plasma.submitBlocks(4, [5, 6]).should.be.rejectedWith(EVMThrow);
     });
 
-    it('should be able to submit blocks signed by operator', async function() {
+    it('should be able to submit blocks signed by operator', async function () {
         //
         // [1] [2] [3]
         //         [3] [4] [5]
         //
 
         {
-            const messageHash = web3.sha3('0x' + [0,1,2,3].map(a => new BigNumber(a).toString(16,64)).reduce((a,b) => a + b), { encoding: 'hex' });
+            const messageHash = web3.sha3('0x' + [0, 1, 2, 3].map(a => new BigNumber(a).toString(16, 64)).reduce((a, b) => a + b), { encoding: 'hex' });
             const rsv = await web3.eth.sign(_, messageHash).substr(2);
-            const r = '0x' + rsv.substr(0,64);
-            const s = '0x' + rsv.substr(64,64);
-            const v = '0x' + rsv.substr(128,2);
-            await plasma.submitBlocksSigned(0, [1,2,3], r, s, v, { from: wallet1 });
+            const r = '0x' + rsv.substr(0, 64);
+            const s = '0x' + rsv.substr(64, 64);
+            const v = '0x' + rsv.substr(128, 2);
+            await plasma.submitBlocksSigned(0, [1, 2, 3], r, s, v, { from: wallet1 });
         }
 
         {
-            const messageHash = web3.sha3('0x' + [2,3,4,5].map(a => new BigNumber(a).toString(16,64)).reduce((a,b) => a + b), { encoding: 'hex' });
+            const messageHash = web3.sha3('0x' + [2, 3, 4, 5].map(a => new BigNumber(a).toString(16, 64)).reduce((a, b) => a + b), { encoding: 'hex' });
             const rsv = await web3.eth.sign(_, messageHash).substr(2);
-            const r = '0x' + rsv.substr(0,64);
-            const s = '0x' + rsv.substr(64,64);
-            const v = '0x' + rsv.substr(128,2);
-            await plasma.submitBlocksSigned(2, [3,4,5], r, s, v, { from: wallet2 });
+            const r = '0x' + rsv.substr(0, 64);
+            const s = '0x' + rsv.substr(64, 64);
+            const v = '0x' + rsv.substr(128, 2);
+            await plasma.submitBlocksSigned(2, [3, 4, 5], r, s, v, { from: wallet2 });
         }
     });
 });
