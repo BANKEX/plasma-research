2c0b8e71dbf8c622ac443b8f7cb9d43629281498
artem <artall64@gmail.com>
2018-10-08 16:07:38 +0300

    Move factory function into separate file, add TODO on test

--- software/blockExplorer/rlp/encoding.go
+++ software/blockExplorer/rlp/encoding.go
@@ -3,17 +3,28 @@ package rlp
 import (
 	"../types"
 	"github.com/ethereum/go-ethereum/rlp"
+	"testing"
 )
 
-//EncodeBlock - will return an encoded block in bytes
+// EncodeBlock - will return an encoded block in bytes
 func EncodeBlock(block *types.Block) []byte {
 	val, _ := rlp.EncodeToBytes(block)
 	return val
 }
 
-//DecodeBlocl - will decode to Block structure from bytes
+// DecodeBlock - will decode to Block structure from bytes
 func DecodeBlock(v []byte) types.Block {
 	b := types.Block{}
 	rlp.DecodeBytes(v, b)
 	return b
+}
+
+
+func TestEncodeBlock(t *testing.T) {
+	// TODO: Step1. Create block, assign value to the fields and save to RLP
+	// TODO: Step2. Compare bytes with sample predefined bytes[]
+}
+
+func TestDecodeBlock(t *testing.T) {
+	// TODO: Get hardcoded RLP values and check that we can successfully decode it
 }
--- software/blockExplorer/types/types.go
+++ software/blockExplorer/types/types.go
@@ -45,76 +45,11 @@ type Output struct {
 }
 
 type Signature struct {
-	Signature [64]byte `json:"signature"`
+	Signature [65]byte `json:"signature"`
 }
 
 type Metadata struct {
 	Max_block_id uint32 `json:"max_block_id"`
 }
 
-func BlockInit() Block {
 
-	metadata := Metadata{}
-	metadata.Max_block_id = 32
-
-	signature := Signature{}
-	slice := []byte("abcdefgh")
-	var arr [64]byte
-	copy(arr[:], slice[:4])
-	signature.Signature = arr
-
-	signatures := make([]Signature, 1)
-	signatures = append(signatures, signature)
-
-	output := Output{}
-	max := new(big.Int)
-	max.Exp(big.NewInt(2), big.NewInt(130), nil).Sub(max, big.NewInt(1))
-	//Generate cryptographically strong pseudo-random between 0 - max
-	n, err := rand.Int(rand.Reader, max)
-	if err != nil {
-		//error handling
-	}
-	output.Owner = *n
-	output.AssetId = *n
-	output.Amount = *n
-
-	outputs := make([]Output, 1)
-	outputs = append(outputs, output)
-	outputs = append(outputs, output)
-
-	input := Input{}
-	input.Amount = *n
-	input.AssetId = *n
-	input.Owner = *n
-	input.OutputIndex = 0
-	input.TxIndex = 0
-	input.BlockIndex = 0
-
-	inputs := make([]Input, 1)
-	inputs = append(inputs, input)
-	inputs = append(inputs, input)
-
-
-	transaction := Transaction{}
-	transaction.Input = &inputs
-	transaction.Output = &outputs
-	transaction.Signatures = &signatures
-	transaction.Metadata = &metadata
-
-	transactions := make([]Transaction, 1)
-	transactions = append(transactions, transaction)
-	transactions = append(transactions, transaction)
-
-	header := BlockHeader{}
-	header.Signature = &signature
-	header.Transactions = &transactions
-	header.PreviousBlockHash = *n
-	header.MerkleRoot = *n
-	header.BlockNumber = 0
-
-	block := Block{}
-	block.Transactions = &transactions
-	block.BlockHeader = &header
-
-	return block
-}
