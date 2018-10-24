cd52d8ec12838022f5b06fb3b3ebb08d030b6f3e
Anton Bukov <k06aaa@gmail.com>
2018-10-25 00:29:23 +0300

    Use 4 spaces instead of 2 spaces

--- software/smartContract/contracts/mocks/MerkleProofWrapper.sol
+++ software/smartContract/contracts/mocks/MerkleProofWrapper.sol
@@ -4,16 +4,16 @@ import { MerkleProof } from "../MerkleProof.sol";
 
 contract MerkleProofWrapper {
 
-  function verify(
-    bytes32[] proof,
-    bytes32 root,
-    bytes32 leaf,
-    uint256 index
-  )
-    public
-    pure
-    returns (bool)
-  {
-    return MerkleProof.verify(proof, root, leaf, index);
-  }
+    function verify(
+        bytes32[] proof,
+        bytes32 root,
+        bytes32 leaf,
+        uint256 index
+    )
+        public
+        pure
+        returns (bool)
+    {
+        return MerkleProof.verify(proof, root, leaf, index);
+    }
 }
--- software/smartContract/test/MerkleProof.js
+++ software/smartContract/test/MerkleProof.js
@@ -7,59 +7,59 @@ require('chai')
 .should();
 
 function ds(size) {
-  it(`should return true for a valid Merkle proof with ${size} elements`, async function () {
-    var elements = []
-    for (var i = 0; i < size; i += 1) {
-      elements.push(`a${i}`)
-    }
-    const merkleTree = new MerkleTree(elements);
-    const root = merkleTree.getHexRoot();
-    for (var i = 0; i < size; i += 1) {
-      const proof = merkleTree.getHexProof(i);
-      const leaf = bufferToHex(merkleTree.layers[0][i]);
-      (await this.merkleProof.verify(proof, root, leaf, i)).should.equal(true);
-    }
-  });
+    it(`should return true for a valid Merkle proof with ${size} elements`, async function () {
+        var elements = []
+        for (var i = 0; i < size; i += 1) {
+            elements.push(`a${i}`)
+        }
+        const merkleTree = new MerkleTree(elements);
+        const root = merkleTree.getHexRoot();
+        for (var i = 0; i < size; i += 1) {
+            const proof = merkleTree.getHexProof(i);
+            const leaf = bufferToHex(merkleTree.layers[0][i]);
+            (await this.merkleProof.verify(proof, root, leaf, i)).should.equal(true);
+        }
+    });
 }
 
 contract('MerkleProof', function () {
-  beforeEach(async function () {
-    this.merkleProof = await MerkleProofWrapper.new();
-  });
-
-  describe('verify', function () {
-    for (var i = 1; i < 32; i += 1) {
-      ds(i)
-    }
-
-    it('should return false for an invalid Merkle proof', async function () {
-      const correctElements = ['a', 'b', 'c'];
-      const correctMerkleTree = new MerkleTree(correctElements);
-
-      const correctRoot = correctMerkleTree.getHexRoot();
-
-      const correctLeaf = bufferToHex(sha3(correctElements[0]));
-
-      const badElements = ['d', 'e', 'f', ''];
-      const badMerkleTree = new MerkleTree(badElements);
-
-      const badProof = badMerkleTree.getHexProof(badElements[0]);
-
-      (await this.merkleProof.verify(badProof, correctRoot, correctLeaf, 0)).should.equal(false);
+    beforeEach(async function () {
+        this.merkleProof = await MerkleProofWrapper.new();
     });
 
-    it('should return false for a Merkle proof of invalid length', async function () {
-      const elements = ['a', 'b', 'c'];
-      const merkleTree = new MerkleTree(elements);
+    describe('verify', function () {
+        for (var i = 1; i < 32; i += 1) {
+            ds(i)
+        }
 
-      const root = merkleTree.getHexRoot();
+        it('should return false for an invalid Merkle proof', async function () {
+            const correctElements = ['a', 'b', 'c'];
+            const correctMerkleTree = new MerkleTree(correctElements);
 
-      const proof = merkleTree.getHexProof(elements[0]);
-      const badProof = proof.slice(0, proof.length - 5);
+            const correctRoot = correctMerkleTree.getHexRoot();
 
-      const leaf = bufferToHex(sha3(elements[0]));
+            const correctLeaf = bufferToHex(sha3(correctElements[0]));
 
-      (await this.merkleProof.verify(badProof, root, leaf, 0)).should.equal(false);
+            const badElements = ['d', 'e', 'f', ''];
+            const badMerkleTree = new MerkleTree(badElements);
+
+            const badProof = badMerkleTree.getHexProof(badElements[0]);
+
+            (await this.merkleProof.verify(badProof, correctRoot, correctLeaf, 0)).should.equal(false);
+        });
+
+        it('should return false for a Merkle proof of invalid length', async function () {
+            const elements = ['a', 'b', 'c'];
+            const merkleTree = new MerkleTree(elements);
+
+            const root = merkleTree.getHexRoot();
+
+            const proof = merkleTree.getHexProof(elements[0]);
+            const badProof = proof.slice(0, proof.length - 5);
+
+            const leaf = bufferToHex(sha3(elements[0]));
+
+            (await this.merkleProof.verify(badProof, root, leaf, 0)).should.equal(false);
+        });
     });
-  });
 });
