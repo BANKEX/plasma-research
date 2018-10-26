3cde4a9e2e1cff8bd22d6fe1f4866c3f7e2ffe69
Anton Bukov <k06aaa@gmail.com>
2018-10-26 15:53:48 +0300

    Add MerkleProof.verify160 method

--- software/smartContract/contracts/MerkleProof.sol
+++ software/smartContract/contracts/MerkleProof.sol
@@ -30,7 +30,6 @@ library MerkleProof {
         returns (bool)
     {
         bytes32 computedHash = leaf;
-
         uint256 currentIndex = index;
         for (uint256 i = 0; i < proof.length; i++) {
             bytes32 proofElement = proof[i];
@@ -41,6 +40,53 @@ library MerkleProof {
             } else {
                 // Hash(current element of the proof + current computed hash)
                 computedHash = keccak256(abi.encodePacked(proofElement, computedHash));
+            }
+
+            currentIndex = currentIndex / 2;
+        }
+
+        // Check if the computed hash (root) is equal to the provided root
+        return computedHash == root;
+    }
+
+    /**
+    * @dev Verifies a Merkle proof proving the existence of a leaf in a Merkle tree. Assumes that each pair of leaves
+    * and each pair of pre-images are sorted.
+    * @param proof Merkle proof containing sibling hashes on the branch from the leaf to the root of the Merkle tree
+    * @param root Merkle root
+    * @param leaf Leaf of Merkle tree
+    */
+    function verify160(
+        bytes proof, 
+        address root, 
+        address leaf,
+        uint256 index
+    ) 
+        internal
+        pure
+        returns (bool)
+    {
+        // Check if proof length is a multiple of 20 bytes (160 bits)
+        if (proof.length % 20 != 0) {
+            return false;
+        }
+
+        address computedHash = leaf;
+        uint256 currentIndex = index;
+        for (uint256 i = 32; i <= proof.length; i += 20) {
+            address proofElement;
+            // solium-disable-next-line security/no-inline-assembly
+            assembly {
+                // Load the current element of the proof
+                proofElement := div(mload(add(proof, i)), 0x1000000000000000000000000)
+            }
+
+            if ((currentIndex & 1) == 0) {
+                // Hash(current computed hash + current element of the proof)
+                computedHash = address(keccak256(abi.encodePacked(computedHash, proofElement)));
+            } else {
+                // Hash(current element of the proof + current computed hash)
+                computedHash = address(keccak256(abi.encodePacked(proofElement, computedHash)));
             }
 
             currentIndex = currentIndex / 2;
