ed187570813f37f35b3a0dd4da81bdc397177ce2
Anton Bukov <k06aaa@gmail.com>
2018-11-07 18:00:28 +0300

    Make Little-Endian and generic size

--- software/smartContract/contracts/Uint2048.sol
+++ software/smartContract/contracts/Uint2048.sol
@@ -2,106 +2,102 @@ pragma solidity ^0.4.24;
 
 
 library Uint2048 {
-  using Uint2048 for uint256[8];
+  using Uint2048 for uint256[LEN];
 
-  function eq(uint256[8] a, uint256[8] b) internal pure returns(bool) {
-    return
-      a[0] == b[0] && // solium-disable-line operator-whitespace
-      a[1] == b[1] &&
-      a[2] == b[2] &&
-      a[3] == b[3] &&
-      a[4] == b[4] &&
-      a[5] == b[5] &&
-      a[6] == b[6] &&
-      a[7] == b[7];
+  uint256 constant public LEN = 8;
+  uint256 constant public MAX_UINT256 = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
+
+  function eq(uint256[LEN] a, uint256[LEN] b) internal pure returns(bool) {
+    for (uint i = 0; i < LEN; i++) {
+      if (a[i] != b[i]) {
+        return false;
+      }
+    }
+    return true;
   }
 
-  function lt(uint256[8] a, uint256[8] b) internal pure returns(bool) {
-    if (a[0] != b[0]) {
-      return a[0] < b[0];
+  function lt(uint256[LEN] a, uint256[LEN] b) internal pure returns(bool) {
+    for (uint i = 0; i < LEN; i++) {
+      if (a[i] != b[i]) {
+        return a[i] < b[i];
+      }
     }
-    if (a[1] != b[1]) {
-      return a[1] < b[1];
-    }
-    if (a[2] != b[2]) {
-      return a[2] < b[2];
-    }
-    if (a[3] != b[3]) {
-      return a[3] < b[3];
-    }
-    if (a[4] != b[4]) {
-      return a[4] < b[4];
-    }
-    if (a[5] != b[5]) {
-      return a[5] < b[5];
-    }
-    if (a[6] != b[6]) {
-      return a[6] < b[6];
-    }
-    if (a[7] != b[7]) {
-      return a[7] < b[7];
-    }
+    return false;
   }
 
-  function gt(uint256[8] a, uint256[8] b) internal pure returns(bool) {
+  function gt(uint256[LEN] a, uint256[LEN] b) internal pure returns(bool) {
     return !eq(a, b) && !lt(a, b);
   }
 
-  function le(uint256[8] a, uint256[8] b) internal pure returns(bool) {
+  function le(uint256[LEN] a, uint256[LEN] b) internal pure returns(bool) {
     return lt(a, b) || eq(a, b);
   }
 
-  function ge(uint256[8] a, uint256[8] b) internal pure returns(bool) {
+  function ge(uint256[LEN] a, uint256[LEN] b) internal pure returns(bool) {
     return !lt(a, b);
   }
 
   // Math
 
-  function add(uint256[8] a, uint256[8] b) internal pure returns(uint256[8] c) {
-    c[7] = a[7] + b[7];
-    c[6] = a[6] + b[6] + (a[7] > c[7] ? 1 : 0);
-    c[5] = a[5] + b[5] + (a[6] > c[6] ? 1 : 0);
-    c[4] = a[4] + b[4] + (a[5] > c[5] ? 1 : 0);
-    c[3] = a[3] + b[3] + (a[4] > c[4] ? 1 : 0);
-    c[2] = a[2] + b[2] + (a[3] > c[3] ? 1 : 0);
-    c[1] = a[1] + b[1] + (a[2] > c[2] ? 1 : 0);
-    c[0] = a[0] + b[0] + (a[1] > c[1] ? 1 : 0);
+  function inverted(uint256[LEN] a) internal pure returns(uint256[LEN] c) {
+    for (uint i = 0; i < LEN; i++) {
+      c[i] = ~a[i];
+    }
   }
 
-  function sub(uint256[8] a, uint256[8] b) internal pure returns(uint256[8] c) {
-    c[7] = a[7] - b[7];
-    c[6] = a[6] - b[6] - (a[7] < c[7] ? 1 : 0);
-    c[5] = a[5] - b[5] - (a[6] < c[6] ? 1 : 0);
-    c[4] = a[4] - b[4] - (a[5] < c[5] ? 1 : 0);
-    c[3] = a[3] - b[3] - (a[4] < c[4] ? 1 : 0);
-    c[2] = a[2] - b[2] - (a[3] < c[3] ? 1 : 0);
-    c[1] = a[1] - b[1] - (a[2] < c[2] ? 1 : 0);
-    c[0] = a[0] - b[0] - (a[1] < c[1] ? 1 : 0);
+  function incremented(uint256[LEN] a) internal pure returns(uint256[LEN]) {
+    for (uint i = 0; i < LEN; i++) {
+      if (a[i] < MAX_UINT256) {
+        a[i] += 1;
+        break;
+      }
+    }
+
+    while (i > 0) {
+      a[--i] = 0;
+    }
+
+    return a;
   }
 
-  function mul(uint256[8] a, uint256[8] b) internal pure returns(uint256[8] c) {
+  function add(uint256[LEN] a, uint256[LEN] b) internal pure returns(uint256[LEN] c) {
+    c[0] = a[0] + b[0];
+    for (uint i = 1; i < LEN; i++) {
+      c[i] = a[i] + b[i] + (a[i - 1] > c[i - 1] ? 1 : 0);
+    }
+  }
+
+  function sub(uint256[LEN] a, uint256[LEN] b) internal pure returns(uint256[LEN] c) {
+    //return a.add(b.inverted().incremented());
+    c[0] = a[0] - b[0];
+    for (uint i = 1; i < LEN; i++) {
+      c[i] = a[i] - b[i] - (a[i - 1] < c[i - 1] ? 1 : 0);
+    }
+  }
+
+  function mul(uint256[LEN] a, uint256[LEN] b) internal pure returns(uint256[LEN] c) {
     // TODO: implement
   }
 
-  function div(uint256[8] a, uint256[8] b) internal pure returns(uint256[8] c) {
+  function div(uint256[LEN] a, uint256[LEN] b) internal pure returns(uint256[LEN] c) {
     // TODO: implement
   }
 
   // Modular
 
-  function mod(uint256[8] a, uint256[8] b) internal pure returns(uint256[8] c) {
+  function mod(uint256[LEN] a, uint256[LEN] b) internal pure returns(uint256[LEN] c) {
     // TODO: implement
   }
 
-  function addmod(uint256[8] a, uint256[8] b, uint256[8] m) internal pure returns(uint256[8] c) {
+  function addmod(uint256[LEN] a, uint256[LEN] b, uint256[LEN] m) internal pure returns(uint256[LEN] c) {
     // TODO: implement
   }
 
-  function mulmod(uint256[8] a, uint256[8] b, uint256[8] m) internal pure returns(uint256[8] c) {
+  function mulmod(uint256[LEN] a, uint256[LEN] b, uint256[LEN] m) internal pure returns(uint256[LEN] c) {
     // TODO: implement
   }
 
-  function powmod(uint256[8] a, uint256 p, uint256[8] m) internal pure returns(uint256[8] c) {
+  function powmod(uint256[LEN] a, uint256 p, uint256[LEN] m) internal pure returns(uint256[LEN] c) {
     // TODO: implement
   }
 }
