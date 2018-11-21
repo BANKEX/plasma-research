28ab9c81fbfe50a11f17f7eb8c334667f5b3c499
Konstantin Privezentsev <privezentsev@gmail.com>
2018-11-21 14:17:26 +0300

    [ISSUE-41] clean code and append documentation

--- software/smartContract/contracts/OrderedIntervalList.sol
+++ software/smartContract/contracts/OrderedIntervalList.sol
@@ -1,77 +1,91 @@
 pragma solidity ^0.4.24;
 
+/**
+    @title OrderedIntervalList
+    @dev List of ordered by intervals with non intersections checks. 
+ */
 library OrderedIntervalList {
     struct Interval {
-        uint64 begin;
-        uint64 end; // exclusion
+        uint64 begin; // inclusive
+        uint64 end; // exclusive
     }
     struct Data {
-        mapping (bytes32 => Interval) intervals;
-        uint64 size;
-
+        Interval[] intervals;
     }
 
-    function getSize(Data storage self) internal view returns(uint64 size) {
-        return self.size;
+    function getSize(Data storage self) internal view returns(uint256 size) {
+        return self.intervals.length;
     }
 
-    function get(Data storage self, uint index) internal returns(uint64 begin, uint64 end) {
-        require(self.size > 0 && index < self.size && index >= 0);
-        Interval memory interval = self.intervals[getIndex(index)];
+    /**
+    @notice Get interval by list index
+    @param _index interval index in list
+    @return interval tuple
+     */
+    function get(Data storage self, uint _index) internal view returns(uint64 begin, uint64 end) {
+        require(self.intervals.length > 0 && _index < self.intervals.length, "check list is non empty and index have proper bounds");
+        Interval memory interval = self.intervals[_index];        
         return (interval.begin, interval.end);    
     }
-    function append(Data storage self, uint64 _begin, uint64 _end) internal returns(bool) {
-        uint64 newIndex = self.size;
-        self.size = newIndex + 1;
-        insert(self, newIndex, _begin, _end);
+
+    /**
+    @notice Append interval to the end of list
+    @param _begin left bound of interval (inclusive)
+    @param _end right bound of interval (exclusive)    
+     */
+    function append(Data storage self, uint64 _begin, uint64 _end) internal {       
+        insert(self, self.intervals.length, _begin, _end);
     }
 
-    function insert(Data storage self, uint64 index, uint64 _begin, uint64 _end) internal returns(bool) {
-        require(_begin <= _end);
-        require(index < self.size);
+    /**
+    @notice Insert interval by specific index in list
+    @dev Method also check that new interval doesn't intersect with existed intervals in list
+    @param _index target index in list
+    @param _begin left bound of interval (inclusive)
+    @param _end right bound of interval (exclusive)        
+     */
+    function insert(Data storage self, uint256 _index, uint64 _begin, uint64 _end) internal {
+        require(_begin < _end, "right bound greater than left bound");      
+        require(_index <= self.intervals.length, "valid index bounds");
         uint prev = 0;
-        bytes32 prevIndex = 0;
+        uint prevIndex = 0;
         uint next = 0;
-        uint64 nextRawIndex = index + 1;
+        uint nextRawIndex = _index + 1;
         
         
-        if (index > 0) {
-            prevIndex = getIndex(index - 1);
+        if (_index > 0) {
+            prevIndex = _index - 1;
             prev = self.intervals[prevIndex].end;
         }        
-
-        nextRawIndex = index + 1;
-        bytes32 nextIndex = 0;
-        while (nextRawIndex < self.size && next == 0) {
-            nextIndex = getIndex(nextRawIndex);
-            Interval memory nextEl = self.intervals[nextIndex];
+        
+        nextRawIndex = _index + 1;        
+        while (nextRawIndex < self.intervals.length && next == 0) {
+            Interval memory nextEl = self.intervals[nextRawIndex];
             if (nextEl.end != 0) {
                 next = nextEl.end;
             }
-            nextRawIndex = nextRawIndex + 1;
-                        
-        }
-        if (nextRawIndex >= self.size) {
-            self.size = nextRawIndex;
-        }
+            nextRawIndex = nextRawIndex + 1;                        
+        }       
 
-        require((prev <= _begin || prev == 0) && (next == 0 || next >= _end));
-        bytes32 curIndexHash = getIndex(index);
-        require(self.intervals[curIndexHash].end == 0, "check free slot");       
+        require((prev <= _begin || prev == 0) && (next == 0 || next >= _end), "valid interval bounds");
 
-        self.intervals[curIndexHash] = Interval({begin: _begin, end: _end});
-        return true;
         
-    }
-    function getIndex(uint index) private returns(bytes32) {
-        return sha3(index);
-    }
+        if (_index == self.intervals.length) {
+            self.intervals.push(Interval({begin: _begin, end: _end}));
+        } else {
+            require(self.intervals[_index].end == 0, "free slot exists");       
+            self.intervals[_index] = Interval({begin: _begin, end: _end});
+        }
+        
+    } 
 
-    function remove(Data storage self, uint64 _index) internal returns(bool) {
-        delete self.intervals[getIndex(_index)];
-        return true;
-    }
-
-    
+    /**
+        @notice Remove interval by index
+        @param _index interval index in list        
+     */
+    function remove(Data storage self, uint64 _index) internal {
+        require(_index < self.intervals.length, "valid index bounds");
+        delete self.intervals[_index];        
+    }    
     
 }
--- software/smartContract/contracts/mocks/OrderedIntervalListWrapper.sol
+++ software/smartContract/contracts/mocks/OrderedIntervalListWrapper.sol
@@ -8,28 +8,28 @@ contract OrderedIntervalListWrapper {
     OrderedIntervalList.Data _data;
 
     function getSize() public view returns(uint _size) {
-        return _data.size;
+        return _data.intervals.length;
     }
 
     
     function get(uint index) public view returns(uint64 begin, uint64 end) {
 
         (begin, end) = _data.get(index);
-        //return (begin, end);
+        return (begin, end);
         
     }
 
-    function append(uint64 _begin, uint64 _end) public returns(bool) {
-        return _data.append(_begin, _end);
+    function append(uint64 _begin, uint64 _end) public {
+        _data.append(_begin, _end);
     }
 
-    function set(uint64 index, uint64 _begin, uint64 _end) public returns(bool) {
-        return _data.insert(index, _begin, _end);
+    function set(uint64 index, uint64 _begin, uint64 _end) public {
+        _data.insert(index, _begin, _end);
         
     }
   
-    function remove( uint64 _index) public returns(bool) {
-        return _data.remove(_index);
+    function remove(uint64 _index) public {
+        _data.remove(_index);
     }
 
 
