568ae74c55f49ef99d52c1b91c5bd3fe632847ef
Kirill <krboktv@gmail.com>
2018-11-25 21:38:37 +0300

    add script for edding contract address

--- software/run.sh
+++ software/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker stop $(docker ps -aq)
-docker run -it -d -p 9545:8545 -p 8546:8546 -p 30303:30303  erage/gethplasma
+docker rm -f geth
+docker run -it -d -p 9545:8545 --name geth -p 8546:8546 -p 30303:30303  erage/gethplasma
 cd smartContract 
 sudo rm -rf build
 truffle migrate --network development
--- software/smartContract/migrations/2_deploy_contracts.js
+++ software/smartContract/migrations/2_deploy_contracts.js
@@ -1,5 +1,17 @@
 const BankexPlasma = artifacts.require('BankexPlasma');
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, '../../client-operator/verifier/config.json');
 
-module.exports = async function (deployer) {
-  deployer.deploy(BankexPlasma);
+
+
+module.exports = function (deployer) {
+    deployer.then(async function () {
+        const contractAddress = await BankexPlasma.new();
+        const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+        data.smart = contractAddress.address;
+        console.log("BankexPlasma address: " + contractAddress.address)
+        fs.writeFileSync(filePath, JSON.stringify(data));
+    });
+
 };
