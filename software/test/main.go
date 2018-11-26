4e163c3d59a5947373458dd3547efaeb61360761
EnoRage <enormousrage@gmail.com>
2018-11-26 05:04:36 +0300

    rewrite run.sh

--- software/test/main.go
+++ software/test/main.go
@@ -9,13 +9,13 @@ import (
 	"fmt"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"io/ioutil"
 	"log"
 	"math/big"
 	"os"
-	"time"
 )
 type Config struct {
 	Port     int    `json:port`
@@ -43,7 +43,7 @@ func OpenConfig(file string) Config {
 }
 
 
-
+var Nonce = 0
 func main () {
 
 	fmt.Println("THIS IS TEST TX FOR EVENT")
@@ -58,10 +58,60 @@ func main () {
 		log.Fatal(err)
 	}
 
-	for i:=0; i < 1000; i++ {
+	for i:=0; i < 100000; i++ {
 		do(client, conf)
-		time.Sleep(time.Second*2)
+		Nonce++
+		//sendTx(client, conf)
+		//Nonce++
+		//time.Sleep(time.Millisecond)
 	}
+}
+
+func sendTx(client *ethclient.Client, conf Config) {
+	privateKey, err := crypto.HexToECDSA("240D6AD83930067D82E0803696996F743ACD78D8FA6A5F6E4F148FD9DEF37C55")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("error casting public key to ECDSA")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	_, _ = client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	value := big.NewInt(100000000) // in wei (1 eth)
+	gasLimit := uint64(21000)                // in units
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	toAddress := common.HexToAddress(conf.Smart)
+	var data []byte
+	tx := types.NewTransaction(uint64(Nonce), toAddress, value, gasLimit, gasPrice, data)
+
+	chainID, err := client.NetworkID(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.SendTransaction(context.Background(), signedTx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
 }
 
 func do(client *ethclient.Client, conf Config) {
@@ -79,7 +129,7 @@ func do(client *ethclient.Client, conf Config) {
 	}
 
 	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
-	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	_, err = client.PendingNonceAt(context.Background(), fromAddress)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -90,10 +140,11 @@ func do(client *ethclient.Client, conf Config) {
 	}
 
 	auth := bind.NewKeyedTransactor(privateKey)
-	auth.Nonce = big.NewInt(int64(nonce))
-	auth.Value = big.NewInt(0)
+	auth.Nonce = big.NewInt(int64(Nonce))
+	auth.Value = big.NewInt(10000000)
 	auth.GasLimit = uint64(300000)
 	auth.GasPrice = gasPrice
+
 
 	address := common.HexToAddress(conf.Smart)
 	instance, err := store.NewStore(address, client)
