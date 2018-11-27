66fd710b9be9aa6584bb6f9501509c36c01449f4
Igor Gulamov <igor.gulamov@gmail.com>
2018-11-27 03:22:22 +0300

    addrsa accumulator with test

---
+++ software/plasmautils/rsa_test.go
@@ -0,0 +1,20 @@
+package primedataset_test
+
+import (
+	"math/big"
+	"testing"
+
+	"./plasmacrypto"
+)
+
+func TestRSAAccumulator(t *testing.T) {
+	a := new(plasmacrypto.Accumulator).SetInt(big.NewInt(17))
+	a.Accumulate(19)
+	res, _ := new(big.Int).SetString("239072435685151324847153", 10)
+	assert(t, a.Value().Cmp(res) == 0, "Wrong accumulator value")
+
+	a = new(plasmacrypto.Accumulator).SetInt(big.NewInt(17))
+	a.BatchAccumulate([]uint32{3, 5})
+	res, _ = new(big.Int).SetString("2862423051509815793", 10)
+	assert(t, a.Value().Cmp(res) == 0, "Wrong accumulator value")
+}
