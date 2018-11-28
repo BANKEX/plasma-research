eb14428603f49dc7ba4c5c995ef8d02a38d35508
Kirill <krboktv@gmail.com>
2018-11-28 19:29:14 +0300

    Rewrite deploy func

--- software/run.sh
+++ software/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This is public address of operator at test geth node
-ADDRESS="0x9B72B510F184e16BcE51Dfd7348Ba474cE30b6ed"
+ADDRESS="0x9b72b510f184e16bce51dfd7348ba474ce30b6ed"
 
 # remove old node at demon
 sudo docker rm -f geth
@@ -10,12 +10,13 @@ sudo docker rm -f geth
 sudo docker run -it -d -p 9545:8545 --name geth -p 8546:8546 -p 30303:30303 erage/gethplasma
 
 cd smartContract
-
-# deploy bankex plasma contract to ethereum
-truffle migrate --network development
-
+sleep 3
 # give plasma owner 100 ether
 ( echo "eth.sendTransaction({from: eth.accounts[0],to:'${ADDRESS}',value: web3.toWei(100, 'ether')})" ) | geth attach http://127.0.0.1:9545
+
+# deploy bankex plasma contract to ethereum
+truffle compile
+node migrations/test_deploy.js
 
 cd ../commons/operator
 
