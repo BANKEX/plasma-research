89f23d4c633ad67f903138257461e38c2ec18c00
EnoRage <enormousrage@gmail.com>
2018-12-12 17:14:51 +0300

    rename software => src

---
+++ src/node/verifier/verifier.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/BANKEX/plasma-research/src/node/verifier/cli"
+	"github.com/BANKEX/plasma-research/src/node/verifier/server"
+)
+
+func main() {
+
+	// go listeners.Checker()
+	// go balance.UpdateBalance(&storage.Balance, conf.Plasma_contract_address)
+	// go event.Start(storage.Client, conf.Plasma_contract_address, &storage.Who, &storage.Amount, &storage.EventBlockHash, &storage.EventBlockNumber)
+	go server.GinServer()
+
+	println("Verifier started")
+
+	cli.CLI()
+
+}
