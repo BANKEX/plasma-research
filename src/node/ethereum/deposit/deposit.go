5de88f7e719d6512e13844676e33224a94205237
Nick Kozlov <Enormousrage@gmail.com>
2018-12-24 16:13:14 +0300

    Erage/features (#57)
    * deposit refactoring
    
    * add deposit and small fixes

--- src/node/ethereum/deposit/deposit.go
+++ src/node/ethereum/deposit/deposit.go
@@ -1,71 +1,65 @@
-package transaction
+package deposit
 
 import (
 	"context"
 	"crypto/ecdsa"
 	"github.com/BANKEX/plasma-research/src/node/ethereum/etherUtils"
+	"github.com/BANKEX/plasma-research/src/node/ethereum/plasmacontract"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"log"
 	"math/big"
 )
 
-func SendTransactionInWei(client *ethclient.Client, privateKey string, value int64, toAddress string, ) (string, error) {
+func Deposit(client *ethclient.Client, privateKey string, contractAddress string, value int64) (string, error) {
 	rawPrivateKey, err := crypto.HexToECDSA(privateKey)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
 
 	publicKey := rawPrivateKey.Public()
 	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
 	if !ok {
-		log.Fatal("error casting public key to ECDSA")
+		log.Println("error casting public key to ECDSA")
 	}
 
 	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		return "", err
+	}
 	err = etherUtils.IsValidAddress(fromAddress)
 	if err != nil {
 		return "", err
 	}
 
-	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
-	if err != nil {
-		return "", err
-	}
-
-	err = etherUtils.IsValidAddress(toAddress)
-	if err != nil {
-		return "", err
-	}
-
-	rawToAddress := common.HexToAddress(toAddress)
-	rawValue := big.NewInt(value)
-	gasLimit := uint64(21000)
 	gasPrice, err := client.SuggestGasPrice(context.Background())
 	if err != nil {
 		return "", err
 	}
 
-	var data []byte
-	tx := types.NewTransaction(nonce, rawToAddress, rawValue, gasLimit, gasPrice, data)
-
-	chainID, err := client.NetworkID(context.Background())
+	auth := bind.NewKeyedTransactor(rawPrivateKey)
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(value)
+	auth.GasLimit = uint64(300000)
+	auth.GasPrice = gasPrice
+	address := common.HexToAddress(contractAddress)
+	err = etherUtils.IsValidAddress(address)
 	if err != nil {
 		return "", err
 	}
 
-	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), rawPrivateKey)
+	instance, err := store.NewStore(address, client)
 	if err != nil {
 		return "", err
 	}
 
-	err = client.SendTransaction(context.Background(), signedTx)
+	tx, err := instance.Deposit(auth)
 	if err != nil {
 		return "", err
 	}
 
-	return signedTx.Hash().Hex(), err
+	return tx.Hash().String(), nil
 }
-
