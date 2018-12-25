ad49af5a5a38e9b907e4887f0b9ef759175d140e
EnoRage <enormousrage@gmail.com>
2018-12-25 22:31:04 +0300

    final fmt fixes

--- src/node/verifier/cli/options/options.go
+++ src/node/verifier/cli/options/options.go
@@ -3,27 +3,27 @@ package options
 import "github.com/c-bata/go-prompt"
 
 var InitialOptions = []prompt.Suggest{
-	prompt.Suggest{Text: "plasma", Description: "Plasma functions"},
-	prompt.Suggest{Text: "eth", Description: "ETH functions"},
-	prompt.Suggest{Text: "main", Description: "Main functions"},
-	prompt.Suggest{Text: "exit", Description: "Exit from app"},
+	{Text: "plasma", Description: "Plasma functions"},
+	{Text: "eth", Description: "ETH functions"},
+	{Text: "main", Description: "Main functions"},
+	{Text: "exit", Description: "Exit from app"},
 }
 
 var PlasmaOptions = []prompt.Suggest{
-	prompt.Suggest{Text: "plasmaBalance", Description: "pb: Get balance of my account in Plasma"},
-	prompt.Suggest{Text: "smartContractAddress", Description: "sca: Get Smart Contract address"},
-	prompt.Suggest{Text: "smartContractBalance", Description: "scb: Get balance of Plasma smart contract"},
-	prompt.Suggest{Text: "events", Description: "e: Get all events"},
+	{Text: "plasmaBalance", Description: "pb: Get balance of my account in Plasma"},
+	{Text: "smartContractAddress", Description: "sca: Get Smart Contract address"},
+	{Text: "smartContractBalance", Description: "scb: Get balance of Plasma smart contract"},
+	{Text: "events", Description: "e: Get all events"},
 }
 
 var EthOptions = []prompt.Suggest{
-	prompt.Suggest{Text: "smartContractAddress", Description: "sca: Get Smart Contract address"},
-	prompt.Suggest{Text: "smartContractBalance", Description: "scb: Get balance of Plasma smart contract"},
-	prompt.Suggest{Text: "transfer", Description: "tr: Transfer -  eth tr 0.4(eth) 0x4ED6..."},
-	prompt.Suggest{Text: "accBalance", Description: "ab: Account Balance: eth ab 0x4ED6..."},
+	{Text: "smartContractAddress", Description: "sca: Get Smart Contract address"},
+	{Text: "smartContractBalance", Description: "scb: Get balance of Plasma smart contract"},
+	{Text: "transfer", Description: "tr: Transfer -  eth tr 0.4(eth) 0x4ED6..."},
+	{Text: "accBalance", Description: "ab: Account Balance: eth ab 0x4ED6..."},
 }
 
 var MainOptions = []prompt.Suggest{
-	prompt.Suggest{Text: "deposit", Description: "dep: Deposit"},
-	prompt.Suggest{Text: "exit", Description: "ex: Exit from plasma"},
+	{Text: "deposit", Description: "dep: Deposit"},
+	{Text: "exit", Description: "ex: Exit from plasma"},
 }
