e1461800f83ff840e925f780e45a370b7a6f0517
prazd <alexokda@list.ru>
2019-01-08 12:42:24 +0300

    read json config

--- src/node/blockchain/sum-merkle-tree_test.go
+++ src/node/blockchain/sum-merkle-tree_test.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	. "github.com/BANKEX/plasma-research/src/node/plasmautils/slice"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
@@ -35,67 +37,88 @@ func TestSMT(t *testing.T) {
 		}
 	}
 
-	data := []byte(`
-	[
-		{
-			"inputs": [
-				{
-					"blockNumber":1,
-					"txNumber":1,
-					"outputNumber":1,
-					"owner":[212, 94, 140, 187, 90, 4, 197, 233, 140, 235, 41, 216, 173, 145, 71, 238, 13, 15, 62, 194],
-					"slice": {
-						"begin": 1,
-						"end": 2
-					}
-				}
+	jsonFile, err := os.Open("../../contracts/test/info.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	jsonFile.Close()
 
-			],
-			"outputs": [
-				{
-					"owner":[212, 94, 140, 187, 90, 4, 197, 233, 140, 235, 41, 216, 173, 145, 71, 238, 13, 15, 62, 194],
-					"slice": {
-						"begin": 1,
-						"end": 2
-					}
-				}
-			],
-			"metadata": {
-				"maxBlockNumber":100
-			}
-		},
-		{
-			"inputs": [
-				{
-					"blockNumber":1,
-					"txNumber":1,
-					"outputNumber":1,
-					"owner":[212, 94, 140, 187, 90, 4, 197, 233, 140, 235, 41, 216, 173, 145, 71, 238, 13, 15, 62, 194],
-					"slice": {
-						"begin": 4,
-						"end": 5
-					}
-				}
+	var d Data
+	json.Unmarshal(byteValue, &d)
 
-			],
-			"outputs": [
-				{
-					"owner":[212, 94, 140, 187, 90, 4, 197, 233, 140, 235, 41, 216, 173, 145, 71, 238, 13, 15, 62, 194],
-					"slice": {
-						"begin": 4,
-						"end": 5
-					}
-				}
-			],
-			"metadata": {
-				"maxBlockNumber":100
-			}
-		}
-	]
-	`)
+	data, err := json.Marshal(d.Info)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//var data Dataw
+	//
+	//json.Unmarshal(byteValue, &data)
+
+	//
+	//data := []byte(`
+	//[
+	//	{
+	//		"inputs": [
+	//			{
+	//				"blockNumber":1,
+	//				"txNumber":1,
+	//				"outputNumber":1,
+	//				"owner":[212, 94, 140, 187, 90, 4, 197, 233, 140, 235, 41, 216, 173, 145, 71, 238, 13, 15, 62, 194],
+	//				"slice": {
+	//					"begin": 1,
+	//					"end": 2
+	//				}
+	//			}
+	//
+	//		],
+	//		"outputs": [
+	//			{
+	//				"owner":[212, 94, 140, 187, 90, 4, 197, 233, 140, 235, 41, 216, 173, 145, 71, 238, 13, 15, 62, 194],
+	//				"slice": {
+	//					"begin": 1,
+	//					"end": 2
+	//				}
+	//			}
+	//		],
+	//		"metadata": {
+	//			"maxBlockNumber":100
+	//		}
+	//	},
+	//	{
+	//		"inputs": [
+	//			{
+	//				"blockNumber":1,
+	//				"txNumber":1,
+	//				"outputNumber":1,
+	//				"owner":[212, 94, 140, 187, 90, 4, 197, 233, 140, 235, 41, 216, 173, 145, 71, 238, 13, 15, 62, 194],
+	//				"slice": {
+	//					"begin": 4,
+	//					"end": 5
+	//				}
+	//			}
+	//
+	//		],
+	//		"outputs": [
+	//			{
+	//				"owner":[212, 94, 140, 187, 90, 4, 197, 233, 140, 235, 41, 216, 173, 145, 71, 238, 13, 15, 62, 194],
+	//				"slice": {
+	//					"begin": 4,
+	//					"end": 5
+	//				}
+	//			}
+	//		],
+	//		"metadata": {
+	//			"maxBlockNumber":100
+	//		}
+	//	}
+	//]
+	//`)
 
 	q := make([]Transaction, 0)
 	json.Unmarshal(data, &q)
+	fmt.Println(q)
 	var sumTree *SumMerkleTree
 
 	txs, err := PrepareLeaves(q)
