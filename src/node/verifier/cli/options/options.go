05952088b701beca6e3aca3002edb8f831cf52ae
prazd <alexokda@list.ru>
2019-01-12 20:10:01 +0300

    fix

--- src/node/verifier/cli/options/options.go
+++ src/node/verifier/cli/options/options.go
@@ -11,10 +11,12 @@ var InitialOptions = []prompt.Suggest{
 var EthOptions = []prompt.Suggest{
 	{Text: "transfer", Description: "tr: Transfer -  eth tr 0.4(eth) 0x4ED6..."},
 	{Text: "balance", Description: "bal: Balance of eth account - eth bal 0x4ED6..."},
+	{Text: "ownerBalance", Description: "obal: Balance of user(config) eth account"},
 }
 
 var PlasmaOptions = []prompt.Suggest{
 	{Text: "deposit", Description: "dep: Deposit"},
+	{Text: "balance", Description: "bal: Plasma balance"},
 	{Text: "transfer", Description: "tr: Transfer -  plasma tr 0.4(eth) 0x4ED6..."},
 	{Text: "utxo", Description: "utxo: List UTXOs from plasma"},
 	{Text: "exit", Description: "ex: Exit from plasma"},
