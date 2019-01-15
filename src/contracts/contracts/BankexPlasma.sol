e302d3f17b93715cadb9c0abf6e7004ea8f25588
Anton Bukov <k06aaa@gmail.com>
2019-01-16 00:24:08 +0300

    Fix linter issues

--- src/contracts/contracts/BankexPlasma.sol
+++ src/contracts/contracts/BankexPlasma.sol
@@ -2,7 +2,6 @@ pragma solidity ^0.5.0;
 
 import { PlasmaAssets } from "./PlasmaAssets.sol";
 
-
+// solium-disable-next-line no-empty-blocks
 contract BankexPlasma is PlasmaAssets {
-
 }
--- src/contracts/test/SumMerkleProof.js
+++ src/contracts/test/SumMerkleProof.js
@@ -1,82 +1,82 @@
-const { BN } = require('openzeppelin-test-helpers');
+// const { BN } = require('openzeppelin-test-helpers');
 var assert = require('assert');
 var fs = require('fs');
 
 var excpectedData = JSON.parse(fs.readFileSync('test/sample-merkle-proof.json'));
 
 const SumMerkleProofWrapper = artifacts.require('SumMerkleProofWrapper');
-const SumMerkleProof = artifacts.require('SumMerkleProof');
+// const SumMerkleProof = artifacts.require('SumMerkleProof');
 
-const be32 = function (x) {
-  return x.toBuffer('be', 32).slice(-4);
-};
+// const be32 = function (x) {
+//   return x.toBuffer('be', 32).slice(-4);
+// };
 
-const be160 = function (x) {
-  return x.toBuffer('be', 32).slice(-20);
-};
+// const be160 = function (x) {
+//   return x.toBuffer('be', 32).slice(-20);
+// };
 
-const hash4 = function (l1, l2, a1, a2) {
-  return new BN(web3.utils.keccak256(Buffer.concat([be32(l1), be32(l2), be160(a1), be160(a2)])).slice(12));
-};
+// const hash4 = function (l1, l2, a1, a2) {
+//   return new BN(web3.utils.keccak256(Buffer.concat([be32(l1), be32(l2), be160(a1), be160(a2)])).slice(12));
+// };
 
-const randBits = function (bits) {
-  let value = new BN();
-  for (let i = 0; i < bits; i++) {
-    if (Math.random() > 0.5) {
-      value = value.or((new BN(2)).pow(new BN(i)));
-    }
-  }
-  return value;
-};
+// const randBits = function (bits) {
+//   let value = new BN();
+//   for (let i = 0; i < bits; i++) {
+//     if (Math.random() > 0.5) {
+//       value = value.or((new BN(2)).pow(new BN(i)));
+//     }
+//   }
+//   return value;
+// };
 
-const genRandProof = function (depth) {
-  const item = randBits(160);
-  const begin = randBits(30);
-  const end = begin.add(randBits(24));
+// const genRandProof = function (depth) {
+//   const item = randBits(160);
+//   const begin = randBits(30);
+//   const end = begin.add(randBits(24));
 
-  let index = 0;
-  let proof = new Buffer('');
-  let curItem = item;
-  let curLength = end.sub(begin);
-  let curLeft = begin;
+//   let index = 0;
+//   let proof = Buffer.alloc('');
+//   let curItem = item;
+//   let curLength = end.sub(begin);
+//   let curLeft = begin;
 
-  for (let i = 0; i < depth; i++) {
-    let b = randBits(1);
-    let ci = randBits(160);
-    let cl = randBits(24);
-    if (i === (depth - 2)) {
-      b = new BN(1);
-      cl = curLeft;
-    }
-    if (i === (depth - 1)) {
-      b = new BN(0);
-      cl = new BN(2 ** 32 - 1).sub(curLength);
-    }
+//   for (let i = 0; i < depth; i++) {
+//     let b = randBits(1);
+//     let ci = randBits(160);
+//     let cl = randBits(24);
+//     if (i === (depth - 2)) {
+//       b = new BN(1);
+//       cl = curLeft;
+//     }
+//     if (i === (depth - 1)) {
+//       b = new BN(0);
+//       cl = new BN(2 ** 32 - 1).sub(curLength);
+//     }
 
-    proof = Buffer.concat([proof, be32(cl), be160(ci)]);
-    index = index.add(b.mul((new BN(2)).pow(i)));
-    if (b === 1) {
-      curItem = hash4(cl, curLength, ci, curItem);
-      curLeft = curLeft.sub(cl);
-      curLength = curLength.add(cl);
-    } else {
-      curItem = hash4(curLength, cl, curItem, ci);
-      curLength = curLength.add(cl);
-    }
-  }
+//     proof = Buffer.concat([proof, be32(cl), be160(ci)]);
+//     index = index.add(b.mul((new BN(2)).pow(i)));
+//     if (b === 1) {
+//       curItem = hash4(cl, curLength, ci, curItem);
+//       curLeft = curLeft.sub(cl);
+//       curLength = curLength.add(cl);
+//     } else {
+//       curItem = hash4(curLength, cl, curItem, ci);
+//       curLength = curLength.add(cl);
+//     }
+//   }
 
-  return {
-    index,
-    begin,
-    end,
-    item,
-    proof,
-    curItem,
-    curLength,
-  };
-};
+//   return {
+//     index,
+//     begin,
+//     end,
+//     item,
+//     proof,
+//     curItem,
+//     curLength,
+//   };
+// };
 
-contract('SumMerkleProofWrapper', function ([_, wallet1, wallet2, wallet3, wallet4, wallet5]) {
+contract('SumMerkleProofWrapper', function () {
   let wrapper;
 
   beforeEach(async function () {
